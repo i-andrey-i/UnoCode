52b37ba76d1dc7ab503df437e649415a3e0306c4
AlexLe024 <a.gorlyshkov@gmail.com>
2025-07-08 17:31:28 +0500

    добавил .gitignore

--- alfa_bank_integration/config.py
+++ alfa_bank_integration/config.py
@@ -1,7 +1,12 @@
-CLIENT_ID = "<ВСТАВИТЬ СЮДА client_id>"
-CLIENT_SECRET = "<ВСТАВИТЬ СЮДА client_secret>"
-CERT_PATH = "certs/Company.cer"
-KEY_PATH = "certs/Company.key"
-TOKEN_URL = "https://baas.alfabank.ru/oidc/token"
-API_BASE_URL = "https://baas.alfabank.ru/api"
-SCOPE = "your_scope_here"  # вставляется банком
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+CLIENT_ID = os.getenv("ALFA_CLIENT_ID")
+CLIENT_SECRET = os.getenv("ALFA_CLIENT_SECRET")
+CERT_PATH = os.getenv("ALFA_CERT_PATH", "certs/Company.cer")
+KEY_PATH = os.getenv("ALFA_KEY_PATH", "certs/Company.key")
+TOKEN_URL = os.getenv("ALFA_TOKEN_URL", "https://baas.alfabank.ru/oidc/token")
+API_BASE_URL = os.getenv("ALFA_API_BASE_URL", "https://baas.alfabank.ru/api")
+SCOPE = os.getenv("ALFA_SCOPE", "")
