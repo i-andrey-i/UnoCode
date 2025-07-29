2a30aa83e6331da4591ed9f388cef76a45a899a5
Voyage <kirillbatalovwork@mail.ru>
2025-07-29 17:48:19 +0500

    delete frontend

--- client/src/SheetContext.tsx
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-export const SheetContext = createContext<{ cells: (string | undefined)[][] }>({
-  cells: [],
-});
--- client/src/pages/Homepage/index.tsx
+++
@@ -1,5 +0,0 @@
-import { Navigate } from "react-router";
-
-export function Homepage() {
-  return <Navigate to="daily" />;
-}
